66e8ee06a11b7b32b23c3672e604e15365f131f4
Lawrence M <lawrencejunzh@gmail.com>
2025-03-21 03:11:45 -0400

    fixed sign up page to route to sign in

--- app/(tabs)/shared-dashboard.tsx
+++ app/(tabs)/shared-dashboard.tsx
@@ -8,30 +8,6 @@ import { Text, View } from '@/components/Themed';
 
 import { useRouter } from 'expo-router';
 
-// export default function TabOneScreen() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Tab One</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//   },
-//   separator: {
-//     marginVertical: 30,
-//     height: 1,
-//     width: '80%',
-//   },
-// });
 
 const router = useRouter();
 
@@ -96,7 +72,7 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#ff0000',
     padding: 20,
   },
   title: {
